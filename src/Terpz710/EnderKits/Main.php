4425b0e1cda55d90e74978f3c3fa4cc0153b5d3b
BlueGamesNetwork <142659461+Terpz710@users.noreply.github.com>
2023-10-13 18:45:39 -0700

    Update Main.php

--- src/Terpz710/EnderKits/Main.php
+++ src/Terpz710/EnderKits/Main.php
@@ -7,20 +7,24 @@ namespace Terpz710\EnderKits;
 use pocketmine\plugin\PluginBase;
 use Terpz710\EnderKits\Command\KitCommand;
 use Terpz710\EnderKits\Command\KitsCommand;
-use Terpz710\EnderKits\Command\VIPCommand;
 use Terpz710\EnderKits\Task\CoolDownTask;
 use Terpz710\EnderKits\Task\CooldownManager;
+use Terpz710\BankNotesPlus\BankNotesPlus;
 
 class Main extends PluginBase {
 
     public function onEnable(): void {
-        $cooldownManager = new CooldownManager($this);
-        $this->getServer()->getCommandMap()->register("kit", new KitCommand($this, $cooldownManager));
-        $this->getServer()->getCommandMap()->register("kits", new KitsCommand($this, $cooldownManager));
-        $this->getServer()->getCommandMap()->register("terpz710vip", new VIPCommand($this));
-        $this->saveResource("kits.yml");
+        $rankSystem = $this->getServer()->getPluginManager()->getPlugin("RankSystem");
+        $bankNotePlugin = $this->getServer()->getPluginManager()->getPlugin("BankNotesPlus");
 
-        $cooldownTask = new CoolDownTask($this);
-        $this->getScheduler()->scheduleRepeatingTask($cooldownTask, 20 * 60);
+        if ($rankSystem !== null && $bankNotePlugin instanceof BankNotesPlus) {
+            $cooldownManager = new CooldownManager($this);
+            $this->getServer()->getCommandMap()->register("kit", new KitCommand($this, $cooldownManager, $bankNotePlugin));
+            $this->getServer()->getCommandMap()->register("kits", new KitsCommand($this, $cooldownManager));
+            $this->saveResource("kits.yml");
+
+            $cooldownTask = new CoolDownTask($this);
+            $this->getScheduler()->scheduleRepeatingTask($cooldownTask, 20 * 60);
+        }
     }
 }
