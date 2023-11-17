484a65b0d179998e054143ceecd36e485a931738
BlueGamesNetwork <142659461+Terpz710@users.noreply.github.com>
2023-11-16 20:27:12 -0800

    1.0.0-FIX

--- src/Terpz710/EnderKits/Main.php
+++ src/Terpz710/EnderKits/Main.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Terpz710\EnderKits;
 
 use pocketmine\plugin\PluginBase;
+
 use Terpz710\EnderKits\Command\KitCommand;
 use Terpz710\EnderKits\Command\KitsCommand;
 use Terpz710\EnderKits\Task\CoolDownTask;
@@ -19,7 +20,7 @@ class Main extends PluginBase {
 
         if ($rankSystem !== null && $bankNotePlugin instanceof BankNotesPlus) {
             $cooldownManager = new CooldownManager($this);
-            $this->getServer()->getCommandMap()->register("kit", new KitCommand($this, $cooldownManager, $bankNotePlugin, true));
+            $this->getServer()->getCommandMap()->register("kit", new KitCommand($this, $cooldownManager, $bankNotePlugin));
             $this->getServer()->getCommandMap()->register("kits", new KitsCommand($this, $cooldownManager));
             $this->saveResource("kits.yml");
 
